894ed32de7bea394a1d0848a45c0dd2dd49afec0
Irina Rogova <buyersbirds@gmail.com>
2020-11-08 16:48:57 +0600

    Changes

--- bonus-service/src/main/java/Main.java
+++ bonus-service/src/main/java/Main.java
@@ -3,14 +3,8 @@ public class Main {
     BonusService service = new BonusService();
     long amount = 1000_60;
     boolean registered = true;
-    long expected = 30;
 
-    long actual = service.calculate(amount, registered);
-
-    boolean passed = expected == actual;
-
-    System.out.println(passed);
-
+    service.calculate(amount, registered);
   }
 }
 
